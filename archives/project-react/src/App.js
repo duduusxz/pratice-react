2887a112014c730eb3fd89508de3c205f1b04141
duduusxz <eduardoataidemelo@gmail.com>
2025-08-08 00:20:27 -0300

    Aprendendo a utilizar props, propriedades que podem ser tilizadas como objetos no JS.

--- archives/project-react/src/App.js
+++ archives/project-react/src/App.js
@@ -1,35 +1,20 @@
 import './App.css';
 import OlaMundo from './components/OlaMundo';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = 'Eduardo'
-  const newName = name.toUpperCase()
-
-  function sum(a, b){
-    return a + b
-  }
-
-  const url = 'https://static.vecteezy.com/ti/fotos-gratis/t2/41436456-ai-gerado-cinematografico-imagem-do-uma-leao-dentro-uma-natureza-panorama-foto.jpg/150'
+  const nome = 'Carlos'
 
   return (
     <div className="App">
-      <h1>Olá, este é meu projeto React</h1>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="leao" />
-      <img src={url} alt="imagem" />
       <OlaMundo />
+      <SayMyName nome='Dom Pedro' />
+      <SayMyName nome={nome} />
+      <Pessoa nome='Miguel' idade='22' profissao='programador' foto='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSMfPVWnPAr3h5M9quC_q8wGg8WbonDm_ljUrcDE-BJKBBg3evm1p8LEA8Z8_PouhDtvdaeimGWUz1vxVo1n0P7x1JPCAqp9524vMtEm39J'/>
     </div>
   );
-}
-
-function Aplicativo(){
-  return(
-    <div>
-      <h1>Olá</h1>
-    </div>
-  )
 }
 
 export default App;
