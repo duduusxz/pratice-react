ea35ce761073614cba53722adbab6781847ff5da
duduusxz <eduardoataidemelo@gmail.com>
2025-08-18 22:56:20 -0300

    aprendendo a usar useState, e usando uma função que joga mensagens do js na tela do usuário como forma de HTML.

--- archives/project-react/src/components/Form.jsx
+++ archives/project-react/src/components/Form.jsx
@@ -1,20 +1,40 @@
+import {useState} from 'react'
+import styles from './Form.module.css'
+
 function Form(){
 
     function cadastrarUser(e){
+        e.preventDefault() 
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${passowrd}`)
+    }
+
+    const [mensagem, setMensagem] = useState("")
+    const [name, setName] = useState("")
+    const [passowrd, setPassword] = useState("")
+
+    function exibirUser(e){
         e.preventDefault()
-        console.log("Usuário cadastrado")
+        setMensagem(`Olá ${name}, sua senha é ${passowrd}`)
     }
 
     return (
-        <div>
+        <div className={styles.containerCadastro}>
             <h1>Meu cadastro:</h1>
-            <form onSubmit={cadastrarUser}>
-                <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+            <form onSubmit={exibirUser}>
+                <div className={styles.formsCadastro}>
+                    <div className={styles.conjuntoInput}>
+                        <label htmlFor="name">Nome:</label>
+                        <input type="text" name="name" id="name" placeholder="Digite seu nome" onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className={styles.conjuntoInput}>
+                        <label htmlFor="password">Senha:</label>
+                        <input type="text" name="password" id="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <div>
+                        <button type="submit">Cadastrar</button>
+                    </div>
                 </div>
-                <div>
-                    <input type="submit" value="Cadastrar" />
-                </div>
+                {mensagem && <p className={styles.mensagem}>{mensagem}</p>}
             </form>
         </div>
     )
