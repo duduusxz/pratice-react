715ee191651a73fcfb5d8f7ce94f93d4cca455ad
duduusxz <eduardoataidemelo@gmail.com>
2025-08-20 01:22:19 -0300

    Fazendo comentários no código do App.jsx.

--- archives/project-react/src/App.jsx
+++ archives/project-react/src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
-import Evento from './components/Eventos'
-import Form from './components/Form'
+import './App.css'  // importa o css global do projeto
+import Evento from './components/Eventos' // importa o componente 'Evento' que está em components
+import Form from './components/Form' // importa todo o componente de 'Form' que está em components, fazendo um formulário de cadastro
 
-function App() {
+function App() { //cria a função app
 
-  const nome = 'Carlos'
+  const nome = 'Carlos' // cria a const nome e atribui um valor a ele
 
   return (
-    <div className="App">
+    <div className="App"> {/*criando uma class para a div, para conseguir mexer com ela no css*/}
       <h1>Testando eventos</h1>
-      <Evento numero="1" />
+      <Evento numero="1" /> {/*puxando a função evento atribuindo um número dentro dele*/}
       <Evento numero="2" />
-      <Form />
+      <Form /> {/*puxando a função From que faz um "cadastro" do usuário*/}
     </div>
   );
 }
 
-export default App;
+export default App; //exportação da function para a função App aparecer e funcionar na tela do user
