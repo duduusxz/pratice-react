4f203df304c25fd0f87e6a6517b8d439c562de8e
duduusxz <eduardoataidemelo@gmail.com>
2025-08-21 22:01:33 -0300

    .

--- archives/project-react/src/components/Form.jsx
+++ archives/project-react/src/components/Form.jsx
@@ -11,7 +11,6 @@ function Form(){
     const [mensagem, setMensagem] = useState("")
     const [name, setName] = useState("")
     const [passowrd, setPassword] = useState("")
-    const [email, setEmail] = useState("")
 
     function exibirUser(e){
         e.preventDefault()
@@ -30,10 +29,6 @@ function Form(){
                     <div className={styles.conjuntoInput}>
                         <label htmlFor="password">Senha:</label>
                         <input type="text" name="password" id="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
-                    </div>
-                    <div className={styles.conjuntoInput}>
-                        <label htmlFor="Email">Email:</label>
-                        <input type="text" name="email" id="email" placeholder="Digite seu emaeil" onChange={(e) => sete...target.value)}/>
                     </div>
                     <div>
                         <button type="submit">Cadastrar</button>
